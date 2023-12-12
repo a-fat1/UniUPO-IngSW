fcdb34824522185a0d40244586abf44f59a23baf
20028464 <20028464@studenti.uniupo.it>
2023-12-12 23:17:12 +0100

    creazione metodi in GestoreNotifiche e creazione file Test

--- codice/src/main/java/Elaborazione/GestoreNotifiche.java
+++ codice/src/main/java/Elaborazione/GestoreNotifiche.java
@@ -18,11 +18,65 @@ public class GestoreNotifiche implements GestoreNotificheInterfaccia
 	public GestoreNotifiche(String host) throws RemoteException, NotBoundException
 	{
 		registry = LocateRegistry.getRegistry(host, 1098); 
-       	 	dbNotifiche = (DbNotificheInterfaccia) registry.lookup("dbNotifiche");
+		dbNotifiche = (DbNotificheInterfaccia) registry.lookup("dbNotifiche");
 	}
 
 	public GestoreNotifiche(DbNotifiche d1) // per testing
 	{
 		dbNotifiche = d1;
+	}
+
+	/**
+	 * RF04: Genera il testo della notifica per un nuovo prodotto.
+	 *
+	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
+	 */
+	public String generaTestoNotificaProdotto(HashMap<String, Object> object) {
+		String testoNotifica = "";
+
+
+		return testoNotifica;
+	}
+
+	/**
+	 * RF04: Genera il testo della notifica per un nuovo ordine.
+	 *
+	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
+	 */
+	public String generaTestoNotificaOrdine(HashMap<String, Object> object) {
+		String testoNotifica = "";
+
+		return testoNotifica;
+	}
+
+	/**
+	 * RF04: Genera il testo della notifica per un avviso.
+	 *
+	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
+	 */
+	public String generaTestoNotificaAvviso(HashMap<String, Object> object) {
+		String testoNotifica = "";
+
+		return testoNotifica;
+	}
+
+	/**
+	 * RF04: Genera il testo della notifica per un nuovo utente.
+	 *
+	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
+	 */
+	public String generaTestoNotificaUtente(HashMap<String, Object> object) {
+		String testoNotifica = "";
+
+		return testoNotifica;
+	}
+
+	/**
+	 * RF04: Inserisce una notifica nel database.
+	 *
+	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
+	 */
+	public void inserimentoNotifica() {
+
 	}
 }
