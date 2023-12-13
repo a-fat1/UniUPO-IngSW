e51adda03881f4654efda345195ca10cecbb3c3b
Luca Benetti <20043903@studenti.uniupo.it>
2023-12-13 23:51:04 +0100

    Bozza MostraForm data

--- codice/src/main/java/UserInterface/UiLista.java
+++ codice/src/main/java/UserInterface/UiLista.java
@@ -3,8 +3,8 @@ package UserInterface;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-import java.rmi.registry.Registry; 
-import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.NotBoundException;
 
@@ -13,8 +13,7 @@ import javax.swing.*;
 import UserInterface.*;
 import Elaborazione.*;
 
-public class UiLista extends JOptionPane implements UiListaInterfaccia
-{
+public class UiLista extends JOptionPane implements UiListaInterfaccia {
 	// registri
 	private Registry registryUI;
 	private Registry registryGestore;
@@ -23,26 +22,66 @@ public class UiLista extends JOptionPane implements UiListaInterfaccia
 	private GestoreRicercheInterfaccia gestoreRicerche;
 
 	// attributi
-	
+	private int codice = 0;
+	private String listaForniture;
+	private String dataInizio;
+	private String dataFine;
+	private int esitoControllo = 0;
+
 	// elementi grafici
-	
-	public UiLista(String hostGestore) throws RemoteException, NotBoundException
-	{
+	private JLabel labelDataInizio;
+	private JLabel labelDataFine;
+	private JTextField fieldDataInizio;
+	private JTextField fieldDataFine;
+	private JPanel panelData;
+
+	public UiLista(String hostGestore) throws RemoteException, NotBoundException {
 		registryUI = LocateRegistry.getRegistry("127.0.0.1", 1100); // default: 1099
-		registryGestore = LocateRegistry.getRegistry(hostGestore, 1099); 
+		registryGestore = LocateRegistry.getRegistry(hostGestore, 1099);
 
 		gestoreRicerche = (GestoreRicercheInterfaccia) registryGestore.lookup("gestoreRicerche");
+		
+		labelDataInizio = new JLabel("Data iniziale");
+		labelDataFine = new JLabel("Data finale");
+		fieldDataInizio = new JTextField("",40);
+		fieldDataFine = new JTextField("",40);
+		panelData = new JPanel();
+		panelData.setLayout(new BoxLayout(panelData, BoxLayout.PAGE_AXIS));
+		panelData.add(labelDataInizio);
+		panelData.add(fieldDataInizio);
+		panelData.add(labelDataFine);
+		panelData.add(fieldDataFine);
+
+	}
+
+	public void avvioListaOrdini() throws RemoteException { // RF11
+	}
+
+	public void avvioListaPagamenti() throws RemoteException { // RF12
+	}
+
+	public void avvioListaForniture() throws RemoteException { // RF13
+	}
+
+	public void mostraErrore(int esitoControllo) {
+
+	}
+
+	public void mostraFormRicercaPerData() {
+		var ricerca = showConfirmDialog(null, panelData, "Ricerca per data",this.OK_OPTION);
+
+		if (ricerca == this.OK_OPTION){
+			var dataInizio = fieldDataInizio.getText();
+			var dataFine = fieldDataFine.getText();
+		}
 	}
 	
-	public void avvioListaOrdini() throws RemoteException
-	{	// RF11
+	public void mostraListaItem(ArrayList<HashMap<String, Object>> listaForniture) {
+
 	}
 
-	public void avvioListaPagamenti() throws RemoteException
-	{	// RF12
+	public void mostraListaData(ArrayList<HashMap<String, Object>> listaForniture) {
+
 	}
 
-	public void avvioListaForniture() throws RemoteException
-	{	// RF13	
-	}
 }
--- codice/src/main/java/UserInterface/UiListaInterfaccia.java
+++ codice/src/main/java/UserInterface/UiListaInterfaccia.java
@@ -1,12 +1,18 @@
 package UserInterface;
 
 import java.rmi.Remote; 
-import java.rmi.RemoteException; 
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap; 
 
 public interface UiListaInterfaccia extends Remote
 {
 	void avvioListaOrdini() throws RemoteException;
 	void avvioListaPagamenti() throws RemoteException;
 	void avvioListaForniture() throws RemoteException;
+	void mostraErrore(int esitoControllo);
+	void mostraFormRicercaPerData();
+	void mostraListaItem(ArrayList<HashMap<String, Object>> listaForniture);
+	void mostraListaData(ArrayList<HashMap<String, Object>> listaForniture);
 }
 
