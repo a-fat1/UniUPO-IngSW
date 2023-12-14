41a8578216845cb2f8bee0fe8839e0ebdf6f513a
20028464 <20028464@studenti.uniupo.it>
2023-12-14 19:48:47 +0100

    implementazione metodi di GestoreNotifiche e relativi test, implementazione iniziale dei metodi di UiNotifica

--- codice/src/test/java/RF04GeneraNotifica.java
+++ codice/src/test/java/RF04GeneraNotifica.java
@@ -4,6 +4,7 @@ import DataBase.DbNotifiche;
 import Elaborazione.GestoreNotifiche;
 import org.junit.Test;
 import java.rmi.RemoteException;
+import java.util.HashMap;
 
 import DataBase.DbUtenti;
 import Elaborazione.GestoreAccessi;
@@ -13,9 +14,84 @@ public class RF04GeneraNotifica {
     DbNotifiche dbNotifiche = new DbNotifiche();
     GestoreNotifiche gestoreNotifiche = new GestoreNotifiche(dbNotifiche);
 
+    /*
+     * Test per verificare il corretto funzionamento del metodo "verificaCorrettezzaDati"
+     */
     @Test
-    public void testVerificaCorrettezzaDati() {
+    public void testVerificaCorrettezzaDati1() throws RemoteException {
+        // data in formato errato
+        assertEquals("errore formato data", gestoreNotifiche.verificaCorrettezzaDati("12-12-12", "11:00", "Nuovo utente: Carla Rossi."));
+        assertEquals("errore formato data", gestoreNotifiche.verificaCorrettezzaDati("2024-20-01", "1111:00", "Nuovo utente: Carla Rossi."));
+        assertEquals("errore formato data", gestoreNotifiche.verificaCorrettezzaDati("2023-20-01", "11:00", ""));
 
+    }
+
+    @Test
+    public void testVerificaCorrettezzaDati2() throws RemoteException {
+        // ora in formato errato
+        assertEquals("errore formato ora", gestoreNotifiche.verificaCorrettezzaDati("2024-12-12", "99:00", "Nuovo utente: Carla Rossi."));
+        assertEquals("errore formato ora", gestoreNotifiche.verificaCorrettezzaDati("2024-12-12", "23:89", "Nuovo utente: Carla Rossi."));
+        assertEquals("errore formato ora", gestoreNotifiche.verificaCorrettezzaDati("2024-12-12", "100:00", ""));
+    }
+
+    @Test
+    public void testVerificaCorrettezzaDati3() throws RemoteException {
+        // data non coerente
+        assertEquals("errore data", gestoreNotifiche.verificaCorrettezzaDati("2020-11-12", "11:00", "Nuovo utente: Carla Rossi."));
+        assertEquals("errore data", gestoreNotifiche.verificaCorrettezzaDati("2020-11-12", "11:00", ""));
+    }
+
+    @Test
+    public void testVerificaCorrettezzaDati4() throws RemoteException {
+        // testo notifica vuoto
+        assertEquals("errore testo notifica", gestoreNotifiche.verificaCorrettezzaDati("2024-11-12", "11:00", ""));
+    }
+
+    @Test
+    public void testVerificaCorrettezzaDati5() throws RemoteException {
+        assertEquals("ok", gestoreNotifiche.verificaCorrettezzaDati("2024-11-12", "11:00", "Nuovo utente: Carla Rossi."));
+    }
+
+    /*
+     * Test per verificare il corretto funzionamento dei metodi "generaTestoNotifica*"
+     */
+    @Test
+    public void testGeneraTestoNotifica1() throws RemoteException {
+        // genera testo prodotto
+        HashMap<String, Object> prodotto = new HashMap<>();
+
+        prodotto.put("tipo", "Libro");
+        prodotto.put("autore", "Nikolaj S. Piskunov");
+        prodotto.put("titolo", "Calcolo Integrale e Differenziale 2");
+
+        assertEquals("Nuovo Libro: Nikolaj S. Piskunov, Calcolo Integrale e Differenziale 2.", gestoreNotifiche.generaTestoNotificaProdotto(prodotto));
+    }
+
+    @Test
+    public void testGeneraTestoNotifica2() throws RemoteException {
+        // genera testo ordine
+        HashMap<String, Object> ordine = new HashMap<>();
+
+        ordine.put("username", "clea99");
+
+        assertEquals("Nuovo Ordine: effettuato da clea99.", gestoreNotifiche.generaTestoNotificaOrdine(ordine));
+    }
+
+    @Test
+    public void testGeneraTestoNotifica3() throws RemoteException {
+        // genera testo avviso
+        assertEquals("Avviso: ", gestoreNotifiche.generaTestoNotificaAvviso());
+    }
+
+    @Test
+    public void testGeneraTestoNotifica4() throws RemoteException {
+        // genera testo utente
+        HashMap<String, Object> utente = new HashMap<>();
+
+        utente.put("nome", "Aldo");
+        utente.put("cognome", "Bruni");
+
+        assertEquals("Nuovo Cliente: Aldo Bruni.", gestoreNotifiche.generaTestoNotificaUtente(utente));
     }
 
 }
