9fcc789e4d8b3e27abbc9920af0d34491fdc4048
20028464 <20028464@studenti.uniupo.it>
2023-12-14 19:48:47 +0100

    implementazione metodi di GestoreNotifiche e relativi test, implementazione iniziale dei metodi di UiNotifica

--- codice/src/main/java/UserInterface/UiNotifica.java
+++ codice/src/main/java/UserInterface/UiNotifica.java
@@ -1,5 +1,7 @@
 package UserInterface;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.ArrayList;
 
@@ -23,10 +25,21 @@ public class UiNotifica extends JOptionPane implements UiNotificaInterfaccia
 	private GestoreNotificheInterfaccia gestoreNotifiche;
 
 	// attributi
+	// RF04: genera notifica (Monfermoso, Magenta Biasina)
 	private HashMap<String, Object> utente;
 	private HashMap<String, Object> prodotto;
 	private HashMap<String, Object> ordine;
+	private HashMap<String, Object> dataScadenza;
+	private HashMap<String, Object> dataPubblicazione;
+	private String esitoVerifica;
+
 	// elementi grafici
+	//RF04: genera notifica (Monfermoso, Magenta Biasina)
+	private JLabel dataLabel;
+	private JLabel testoLabel;
+	private JTextField dataField;
+	private JTextField testoField;
+	private JPanel modificaNotificaPanel;
 	
 	public UiNotifica(String hostGestore) throws RemoteException, NotBoundException
 	{
@@ -45,9 +58,28 @@ public class UiNotifica extends JOptionPane implements UiNotificaInterfaccia
 	 *
 	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
 	 */
-	public void avvioGeneraNotifica() throws RemoteException
+	public void avvioGeneraNotifica(String tipoNotifica, HashMap<String, Object> prodotto, HashMap<String, Object> ordine, HashMap<String, Object> utente) throws RemoteException
 	{
+		String testoNotifica = "";
 
+		switch (tipoNotifica) {
+			case "nuovo prodotto":
+				testoNotifica = gestoreNotifiche.generaTestoNotificaProdotto(prodotto);
+				mostraFormNotifica(testoNotifica);
+				break;
+			case "nuovo ordine":
+				testoNotifica = gestoreNotifiche.generaTestoNotificaOrdine(ordine);
+				gestoreNotifiche.inserimentoNotifica(setDataPubblicazione(), dataScadenza, testoNotifica, utente.get("tipoUtente").toString());
+				break;
+			case "nuovo utente":
+				testoNotifica = gestoreNotifiche.generaTestoNotificaUtente(utente);
+				gestoreNotifiche.inserimentoNotifica(setDataPubblicazione(), dataScadenza, testoNotifica, utente.get("tipoUtente").toString());
+				break;
+			case "avviso":
+				testoNotifica = gestoreNotifiche.generaTestoNotificaAvviso();
+				mostraFormNotifica(testoNotifica);
+				break;
+		}
 	}
 
 	/**
@@ -55,7 +87,7 @@ public class UiNotifica extends JOptionPane implements UiNotificaInterfaccia
 	 *
 	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
 	 */
-	private void mostraFormNotifica() {
+	private void mostraFormNotifica(String testoNotifica) {
 
 	}
 
@@ -69,7 +101,29 @@ public class UiNotifica extends JOptionPane implements UiNotificaInterfaccia
 	 * @author  Linda Monfermoso, Gabriele Magenta Biasina
 	 */
 	private void mostraErrore(String tipoErrore) {
+		String messaggio = "";
 
+		switch(tipoErrore) {
+			case "errore formato data":
+				messaggio = "La data fornita non è in formato YYYY-MM-DD.";
+				break;
+			case "errore formato ora":
+				messaggio = "La ora fornita non è in formato HH:mm.";
+				break;
+			case "errore data":
+				messaggio = "La data fornita non è compatibile con la data di pubblicazione.";
+				break;
+			case "errore testo notifica":
+				messaggio = "Il testo della notifica non può essere vuoto.";
+				break;
+		}
+	}
+
+	private HashMap<String, Object> setDataPubblicazione() {
+		dataPubblicazione.put("data", LocalDate.now());
+		dataPubblicazione.put("ora", LocalTime.now());
+
+		return dataPubblicazione;
 	}
 
 }
