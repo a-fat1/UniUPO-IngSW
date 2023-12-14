3300835fb53d9f19107a1829cd109ae8f340ae91
Marco Brivio <20047184@studenti.uniupo.it>
2023-12-14 12:06:24 +0000

    Modifica RF23AggiornaUsername.java

--- codice/src/test/java/RF23AggiornaUsername.java
+++ codice/src/test/java/RF23AggiornaUsername.java
@@ -22,22 +22,22 @@ public class RF23AggiornaUsername {
     }
 
     @Test
-    public void testVerifica0(){
+    public void testVerifica0() throws RemoteException{
         assertEquals(0, gestoreAccessi.verifica("sirio", "Marco"));
     }
 
     @Test
-    public void testVerifica1(){
+    public void testVerifica1() throws RemoteException{
         assertEquals(1, gestoreAccessi.verifica("aldo", "ab"));
     }
 
     @Test
-    public void testVerfica2(){
+    public void testVerfica2() throws RemoteException{
         assertEquals(2, gestoreAccessi.verifica("aldo", "aldo"));
     }
 
     @Test
-    public void testVerifica3(){
+    public void testVerifica3() throws RemoteException{
         assertEquals(3, gestoreAccessi.verifica("ab", "ab"));
     }
 }
