1d6ebc96a5508a24fdeb72d1de70572c4015ec5a
Marco Brivio <20047184@studenti.uniupo.it>
2023-12-14 10:14:05 +0000

    Upload Test dei metodi aggiunti sulla parte di elaborazione

---
+++ codice/src/test/java/RF23AggiornaUsername.java
@@ -0,0 +1,43 @@
+//RF23 - Aggiorna Username
+//autore: Brivio Marco, Serio Giulia
+import static org.junit.Assert.*; 
+import org.junit.Test;
+import java.rmi.RemoteException;
+
+import DataBase.DbUtenti;
+import Elaborazione.GestoreAccessi;
+
+public class RF23AggiornaUsername {
+    DbUtenti dbUtenti = new DbUtenti();
+    GestoreAccessi gestoreAccessi = new GestoreAccessi(dbUtenti);
+
+    @Test
+    public void testVerificaDuplicatoTrue() throws RemoteException{
+        assertEquals(true, gestoreAccessi.verificaDuplicato(""));
+    }
+
+    @Test
+    public void testVerificaDuplicatoFalse() throws RemoteException{
+        assertEquals(false, gestoreAccessi.verificaDuplicato("aldo"));
+    }
+
+    @Test
+    public void testVerifica0(){
+        assertEquals(0, gestoreAccessi.verifica("sirio", "Marco"));
+    }
+
+    @Test
+    public void testVerifica1(){
+        assertEquals(1, gestoreAccessi.verifica("aldo", "ab"));
+    }
+
+    @Test
+    public void testVerfica2(){
+        assertEquals(2, gestoreAccessi.verifica("aldo", "aldo"));
+    }
+
+    @Test
+    public void testVerifica3(){
+        assertEquals(3, gestoreAccessi.verifica("ab", "ab"));
+    }
+}
