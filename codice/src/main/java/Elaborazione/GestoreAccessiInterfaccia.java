482ef16c464b8109419b2a62f6a8ae470b379261
Marco Brivio <20047184@studenti.uniupo.it>
2023-12-14 10:11:26 +0000

    Modifica GestoreAccessiInterfaccia.java con metodi del nostro requisiti

--- codice/src/main/java/Elaborazione/GestoreAccessiInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreAccessiInterfaccia.java
@@ -11,4 +11,13 @@ public interface GestoreAccessiInterfaccia extends Remote
    String controllaFormatoCredenziali(String username, String password) throws RemoteException;
    String ricercaCredenziali(String username, String password) throws RemoteException;
    HashMap<String, Object> ricercaUtente(String username) throws RemoteException;
+   boolean verificaDuplicato(String nuovoUsername) throws RemoteException;
+   int verifica(String vecchioUsername, String nuovoUsername) throws RemoteException;
+   void cambio(String vecchioUsername, String nuovoUsername) throws RemoteException;
+   void mostraErrore(int val);
+   void messaggio(String messaggio);
 } 
+
+
+
+
