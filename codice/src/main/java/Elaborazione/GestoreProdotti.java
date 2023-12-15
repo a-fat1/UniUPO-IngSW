13131322c4a930483c048568e9bf566cd5cb61fb
Kristian Rigo <rigo.kristian@gmail.com>
2023-12-15 16:53:07 +0100

    Aggiunto Test per Nuova Fornitura e aggiunti metodi in GestoreProdotti

--- codice/src/main/java/Elaborazione/GestoreProdotti.java
+++ codice/src/main/java/Elaborazione/GestoreProdotti.java
@@ -1,5 +1,9 @@
 package Elaborazione;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.format.ResolverStyle;
 import java.util.HashMap;
 import java.util.ArrayList;
 
@@ -24,5 +28,32 @@ public class GestoreProdotti implements GestoreProdottiInterfaccia
 	public GestoreProdotti(DbProdotti d1) // per testing
 	{
 		dbProdotti = d1;
+	}
+
+	public HashMap<String, Boolean> controlloDatiFornitura (String data, Float costo, Integer quantita) {
+		HashMap<String, Boolean> esitoControllo = new HashMap<String, Boolean>();
+		for(String s : new String[] {"esitoData", "esitoCosto", "esitoQuantita"})
+			esitoControllo.put(s, null);
+
+		if(data != null && !data.isEmpty()) {
+			try {
+				String dataOra = data + " " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+				LocalDateTime dataFornitura = LocalDateTime.parse(dataOra, DateTimeFormatter.ofPattern("uuuu-MM-dd HH:mm:ss").withResolverStyle(ResolverStyle.STRICT));
+				esitoControllo.put("esitoData", !dataFornitura.isAfter(LocalDateTime.now()));
+			}
+			catch(DateTimeParseException e) {
+				esitoControllo.put("esitoData", false);
+			}
+		}
+		if(costo != null) esitoControllo.put("esitoCosto", Float.compare(costo, 0.0f) > 0);
+		if(quantita != null) esitoControllo.put("esitoQuantita", quantita > 0);
+
+		return esitoControllo;
+	}
+
+	public void aggiungiFornitura(Integer codProdotto, String data, Float costo, Integer quantita){
+		DbProdotti db = new DbProdotti();
+		db.update("INSERT INTO Fornitura (codiceProdotto, dataFornitura, costo, quantita) VALUES ("+codProdotto+", '"+data+"', "+costo+", "+quantita+")");
+		db.update("UPDATE Prodotto SET quantita = quantita + "+quantita+" WHERE codice = "+codProdotto);
 	}
 }
