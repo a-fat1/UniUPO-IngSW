0f4ba6781792054f0cd085f2767ba1694cd67558
Marco Vecchio <20044258@studenti.uniupo.it>
2023-12-15 12:57:41 +0100

    RF11 implementazione interfaccia della ricerca Ordine per prodotto e per utente

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -7,5 +7,9 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public interface GestoreRicercheInterfaccia extends Remote 
-{  
+{
+    //RF11: lista ordini
+    //autore: Marino & Vecchio
+    public ArrayList<HashMap<String, Object>> ricercaPerUtente(String username) throws RemoteException;
+    public ArrayList<HashMap<String, Object>> ricercaPerProdotto(int codiceProdotto) throws RemoteException;
 } 
