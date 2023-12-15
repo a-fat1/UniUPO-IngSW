933b79b9d8f67c03c81b48320c30e15b1995c2a1
Dario Guidotti <20044165@studenti.uniupo.it>
2023-12-15 16:49:07 +0000

    Replace GestoreRicercheInterfaccia.java

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -1,11 +1,15 @@
 package Elaborazione;
 
-import java.rmi.Remote; 
-import java.rmi.RemoteException;  
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import java.util.HashMap;
 import java.util.ArrayList;
 
-public interface GestoreRicercheInterfaccia extends Remote 
-{  
-} 
+public interface GestoreRicercheInterfaccia extends Remote {
+    // compilare qui le firme - metodo normale vuoto
+    public int controlloGiacenza(String giacenza) throws RemoteException;
+
+    public ArrayList<HashMap<String, Object>> ricercaProdotti(String giacenza) throws RemoteException;
+
+}
