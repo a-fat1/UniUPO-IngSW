40bdd4718c2ed061878b82ffd5b424716eff5a8d
Matteo Cartieri <20044003@studenti.uniupo.it>
2023-12-16 10:28:41 +0100

    RF12 aggiunti test controllo parametri e ricerca per data

---
+++ codice/src/test/java/RF12ListaPagamenti.java
@@ -0,0 +1,67 @@
+import static org.junit.Assert.*;
+
+import Elaborazione.GestoreAccessi;
+import org.junit.Test;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import DataBase.DbUtenti;
+import DataBase.DbProdotti;
+import Elaborazione.GestoreRicerche;
+
+public class RF12ListaPagamenti
+{
+    DbUtenti dbUtenti = new DbUtenti();
+    DbProdotti dbProdotti = new DbProdotti();
+    GestoreRicerche gestoreRicerche = new GestoreRicerche(dbUtenti, dbProdotti);
+
+    @Test
+    public void testControlloParametri1()
+    {
+        // formato dataInizio non corretto
+        assertEquals(1, gestoreRicerche.controlloParametri("12/14/2022", "01/12/2023"));
+    }
+
+    @Test
+    public void testControlloParametri2()
+    {
+        // formato dataFine non corretto
+        assertEquals(1, gestoreRicerche.controlloParametri("12/12/2023", "01/13/2023"));
+    }
+
+    @Test
+    public void testControlloParametri3()
+    {
+        // dataInizio > dataFine
+        assertEquals(2, gestoreRicerche.controlloParametri("12/02/2023", "01/01/2023"));
+    }
+
+    @Test
+    public void testControlloParametri4()
+    {
+        // formato dataInizio non corretto
+        assertEquals(0, gestoreRicerche.controlloParametri("12/01/2023", "20/01/2023"));
+    }
+
+    @Test
+    public void testricercaListaPagamentiData1() throws RemoteException
+    {
+        assertFalse(gestoreRicerche.ricercaListaPagamentiData("01/12/2023","31/12/2023").isEmpty());
+        assertEquals(gestoreRicerche.ricercaListaPagamentiData("01/12/2023","31/12/2023").size(),2);
+    }
+
+    @Test
+    public void testricercaListaPagamentiData2() throws RemoteException
+    {
+        assertFalse(gestoreRicerche.ricercaListaPagamentiData("01/12/2023","05/12/2023").isEmpty());
+        assertEquals(gestoreRicerche.ricercaListaPagamentiData("01/12/2023","05/12/2023").size(),1);
+    }
+
+    @Test
+    public void testricercaListaPagamentiData3() throws RemoteException
+    {
+        assertTrue(gestoreRicerche.ricercaListaPagamentiData("01/12/2023","04/12/2023").isEmpty());
+        assertEquals(gestoreRicerche.ricercaListaPagamentiData("01/12/2023","04/12/2023").size(),0);
+    }
+}
