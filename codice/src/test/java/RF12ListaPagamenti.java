1ad9346ef368fbfea5696cf71bbf0566872f0fb9
Matteo Cartieri <20044003@studenti.uniupo.it>
2023-12-18 16:25:31 +0100

    RF12 correzione test

--- codice/src/test/java/RF12ListaPagamenti.java
+++ codice/src/test/java/RF12ListaPagamenti.java
@@ -17,28 +17,28 @@ public class RF12ListaPagamenti
     GestoreRicerche gestoreRicerche = new GestoreRicerche(dbUtenti, dbProdotti);
 
     @Test
-    public void testControlloParametri1()
+    public void testControlloParametri1() throws RemoteException
     {
         // formato dataInizio non corretto
         assertEquals(1, gestoreRicerche.controlloParametri("12/14/2022", "01/12/2023"));
     }
 
     @Test
-    public void testControlloParametri2()
+    public void testControlloParametri2() throws RemoteException
     {
         // formato dataFine non corretto
         assertEquals(1, gestoreRicerche.controlloParametri("12/12/2023", "01/13/2023"));
     }
 
     @Test
-    public void testControlloParametri3()
+    public void testControlloParametri3() throws RemoteException
     {
         // dataInizio > dataFine
         assertEquals(2, gestoreRicerche.controlloParametri("12/02/2023", "01/01/2023"));
     }
 
     @Test
-    public void testControlloParametri4()
+    public void testControlloParametri4() throws RemoteException
     {
         // formato dataInizio non corretto
         assertEquals(0, gestoreRicerche.controlloParametri("12/01/2023", "20/01/2023"));
