973527e01078f67588176d3e0200249f1a0a9fd2
Michele Filidoro <michele.filidoro@gmail.com>
2023-12-18 16:54:57 +0100

    RF10

--- codice/src/main/java/Elaborazione/GestoreProdottiInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreProdottiInterfaccia.java
@@ -10,4 +10,8 @@ public interface GestoreProdottiInterfaccia extends Remote
 {
     HashMap<String, Boolean> controlloDatiFornitura(String data, Float costo, Integer quantita) throws RemoteException;
     void aggiungiFornitura(Integer codProdotto, String data, Float costo, Integer quantita) throws RemoteException;
+
+    void rimuoviProdotto(Integer codProdotto) throws RemoteException;
+
+    void ripristinaProdotto(Integer codProdotto) throws RemoteException;
 } 
