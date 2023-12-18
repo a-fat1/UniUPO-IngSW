7dfe72137386f11c864b2f78d08be812177da4ac
Riccardo Nazzari <20044680@studenti.uniupo.it>
2023-12-18 14:14:52 +0100

    Modifiche ai test delle funzioni

--- codice/src/test/java/RF19RicercaUtente.java
+++ codice/src/test/java/RF19RicercaUtente.java
@@ -15,11 +15,11 @@ public class RF19RicercaUtente {
     DbProdotti dbprodotti = new DbProdotti();
     GestoreRicerche gestorericerche = new GestoreRicerche(dbutenti, dbprodotti);
 
-    // -------------------------------------------------------------
+    // ------------------------------------------------------------------------------------ //
 
     @Test
     public void testControlloParametriUsername(){
-        // controllo username lunghezza inferiore a 3
+        // controllo di username con lunghezza inferiore a 3
         assertEquals(1, gestorericerche.controlloParametri(""));
         assertEquals(1, gestorericerche.controlloParametri("a"));
         assertEquals(1, gestorericerche.controlloParametri("ab"));
@@ -27,14 +27,14 @@ public class RF19RicercaUtente {
 
     @Test
     public void testControlloParametriUsername1(){
-        // controllo username lunghezza corretta
+        // controllo di username con lunghezza corretta
         assertEquals(4, gestorericerche.controlloParametri("abc"));
         assertEquals(4, gestorericerche.controlloParametri("orchidea1"));
     }
 
     @Test
     public void testControlloParametriNomeCognome0(){
-        // controllo nome lunghezza inferiore a 3
+        // controllo di nome con una lunghezza inferiore a 3
         assertEquals(2, gestorericerche.controlloParametri("", "abc"));
         assertEquals(2, gestorericerche.controlloParametri("a", "abc"));
         assertEquals(2, gestorericerche.controlloParametri("ab", "abc"));
@@ -42,14 +42,14 @@ public class RF19RicercaUtente {
 
     @Test
     public void testControlloParametriNomeCognome1(){
-        // controllo nome e cognome con lunghezza corretta
+        // controllo di nome e cognome con lunghezza corretta
         assertEquals(4, gestorericerche.controlloParametri("abc", "abc"));
         assertEquals(4, gestorericerche.controlloParametri("Mario", "Rossi"));
     }
 
     @Test
     public void testControlloParametriNomeCognome2() {
-        // controllo cognome lunghezza inferiore a 3
+        // controllo di cognome con una lunghezza inferiore a 3
         assertEquals(3, gestorericerche.controlloParametri("abc", ""));
         assertEquals(3, gestorericerche.controlloParametri("abc", "a"));
         assertEquals(3, gestorericerche.controlloParametri("abc", "ab"));
@@ -57,20 +57,21 @@ public class RF19RicercaUtente {
 
     @Test
     public void testControlloParametriNomeCognome8(){
-        // controllo nome caratteri non consentiti
+        // controllo di nome con caratteri non consentiti
         assertEquals(2, gestorericerche.controlloParametri("abc123456789!?=)(", "abc"));
     }
 
     @Test
     public void testControlloParametriNomeCognome9(){
-        // controllo cognome caratteri non consentiti
+        // controllo di cognome con caratteri non consentiti
         assertEquals(3, gestorericerche.controlloParametri("abc", "abc123456789!?=)("));
     }
 
 
     @Test
     public void testDatabase0() throws RemoteException {
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteBloccatoNonBloccato("ald");
+        // ricerca di un utente tramite username nel database
+        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteBloccatoNonBloccato("aldo");
         assertEquals("Aldo", utenti.get(0).get("nome"));
         assertEquals("Bianchi", utenti.get(0).get("cognome"));
         assertEquals("aldo", utenti.get(0).get("username"));
@@ -78,7 +79,8 @@ public class RF19RicercaUtente {
 
     @Test
     public void testDatabase1() throws RemoteException {
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteBloccatoNonBloccato("Ald", "Bia");
+        // ricerca di un utente tramite nome e cognome nel database
+        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteBloccatoNonBloccato("Aldo", "Bianchi");
         assertEquals("Aldo", utenti.get(0).get("nome"));
         assertEquals("Bianchi", utenti.get(0).get("cognome"));
         assertEquals("aldo", utenti.get(0).get("username"));
@@ -86,79 +88,40 @@ public class RF19RicercaUtente {
 
     @Test
     public void testDatabase2() throws RemoteException {
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("ald");
+        // ricerca di un utente tramite username nel database
+        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("aldo");
         assertEquals("Aldo", utenti.get(0).get("nome"));
         assertEquals("Bianchi", utenti.get(0).get("cognome"));
         assertEquals("aldo", utenti.get(0).get("username"));
+        assertEquals(1, utenti.get(0).get("attivo"));
     }
 
     @Test
     public void testDatabase3() throws RemoteException {
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("Ald", "Bia");
+        // ricerca di un utente tramite nome e cognome nel database
+        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("Aldo", "Bianchi");
         assertEquals("Aldo", utenti.get(0).get("nome"));
         assertEquals("Bianchi", utenti.get(0).get("cognome"));
         assertEquals("aldo", utenti.get(0).get("username"));
+        assertEquals(1, utenti.get(0).get("attivo"));
     }
 
     @Test
     public void testDatabase4() throws RemoteException {
-        // controllo nessun elemento nell'arrayList
+        // ricerca di un utente bloccato tramite username nel database
+        // controllo che non sia presente nulla (gli utenti bloccati vengono scartati dalla funzione)
         ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("alfio");
         assertEquals(0, utenti.size());
+
     }
 
-    /*
 
     @Test
     public void testDatabase5() throws RemoteException {
-        // controllo numero corretto di elementi da username vuoto per utenti bloccati e non bloccati
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteBloccatoNonBloccato("");
-        assertEquals(9, utenti.size());
-    }
-
-     */
-
-    @Test
-    public void testDatabase5() throws RemoteException {
-        // controllo un solo elemento nell'arrayList
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteBloccatoNonBloccato("alfio");
-        assertEquals(1, utenti.size());
-        assertEquals(0, utenti.get(0).get("attivo"));
-    }
-
-    @Test
-    public void testDatabase6() throws RemoteException {
+        // ricerca di un utente bloccato tramite nome e cognome nel database
+        // controllo che non sia presente nulla (gli utenti bloccati vengono scartati dalla funzione)
         ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("alfio");
         assertEquals(utenti.size(), 0);
     }
 
-    /*
-    @Test
-    public void testDatabase4() throws RemoteException {
-        // controllo numero corretto di elementi per utenti non bloccati (da username vuoto)
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("");
-        assertEquals(8, utenti.size());
-    }
-
-     */
-
-    /*
-
-    @Test
-    public void testDatabase() throws RemoteException {
-        // Ricerca con utenti multipli
-        ArrayList<HashMap<String, Object>> utenti = gestorericerche.cercaUtenteNonBloccato("s");
-        assertEquals(3, utenti.size());
-        assertEquals("Sirio", utenti.get(0).get("nome"));
-        assertEquals("Smith", utenti.get(0).get("cognome"));
-        assertEquals("sirio", utenti.get(0).get("username"));
-        assertEquals("Silvio", utenti.get(1).get("nome"));
-        assertEquals("Taylor", utenti.get(1).get("cognome"));
-        assertEquals("sisto", utenti.get(1).get("username"));
-        assertEquals("Sara", utenti.get(2).get("nome"));
-        assertEquals("Keynes", utenti.get(2).get("cognome"));
-        assertEquals("soave", utenti.get(2).get("username"));
-    }
-
-     */
 }
