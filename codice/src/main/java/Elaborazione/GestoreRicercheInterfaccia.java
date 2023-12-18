93b29324828599a6211fc52821be46ebd23017af
Alessandro Fatone <20019780@studenti.uniupo.it>
2023-12-18 19:50:25 +0000

    Aggiornato GestoreRicercheInterfaccia

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 
 public interface GestoreRicercheInterfaccia extends Remote 
 {
-
     //RF11: lista ordini
     //autore: Marino & Vecchio
     public ArrayList<HashMap<String, Object>> ricercaPerUtente(String username) throws RemoteException;
@@ -16,18 +15,18 @@ public interface GestoreRicercheInterfaccia extends Remote
     public int controlloParametri(String dataInizio, String dataFine) throws RemoteException;
     public ArrayList<HashMap<String, Object>> ricercaListaPagamentiData(String dataInizio, String dataFine) throws RemoteException;
 
+    // RF18 Prodotti in esaurimento
+    // Alessandro Fatone, Dario Guidotti
+    int controlloGiacenza(String giacenza) throws RemoteException;
+	ArrayList<HashMap<String, Object>> ricercaProdotti(String giacenza) throws RemoteException;
+
     // RF19 RicercaUtente
     // Nazzari Riccardo, Andrea Benedetto
-
     int controlloParametriRicercaUtente(String username) throws RemoteException;
     int controlloParametriRicercaUtente(String nome, String cognome) throws RemoteException;
-
     ArrayList<HashMap<String, Object>> cercaUtenteBloccatoNonBloccato(String username) throws RemoteException;
-
     ArrayList<HashMap<String, Object>> cercaUtenteNonBloccato(String username) throws RemoteException;
-
     ArrayList<HashMap<String, Object>> cercaUtenteBloccatoNonBloccato(String nome, String cognome) throws RemoteException;
-
     ArrayList<HashMap<String, Object>> cercaUtenteNonBloccato(String nome, String cognome) throws RemoteException;
 } 
 
