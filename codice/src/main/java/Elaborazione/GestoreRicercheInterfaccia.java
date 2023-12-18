4840bc5ed0c1546d3e2691bd443e69965dbb6e7f
Riccardo Nazzari <20044680@studenti.uniupo.it>
2023-12-18 14:14:20 +0100

    Aggiunti prototipi delle funzioni

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -7,5 +7,18 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 public interface GestoreRicercheInterfaccia extends Remote 
-{  
+{
+    // RF19 RicercaUtente
+    // Nazzari Riccardo, Andrea Benedetto
+
+    int controlloParametri(String username);
+    int controlloParametri(String nome, String cognome);
+
+    ArrayList<HashMap<String, Object>> cercaUtenteBloccatoNonBloccato(String username) throws RemoteException;
+
+    ArrayList<HashMap<String, Object>> cercaUtenteNonBloccato(String username) throws RemoteException;
+
+    ArrayList<HashMap<String, Object>> cercaUtenteBloccatoNonBloccato(String nome, String cognome) throws RemoteException;
+
+    ArrayList<HashMap<String, Object>> cercaUtenteNonBloccato(String nome, String cognome) throws RemoteException;
 } 
