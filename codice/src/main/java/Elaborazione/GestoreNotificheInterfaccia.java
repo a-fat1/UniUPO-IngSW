6553275a192b63dfcd6ff43122c5821f937c16f3
Diego Galletti <20044195@studenti.uniupo.it>
2023-12-19 10:29:11 +0000

    aggiornato codice

--- codice/src/main/java/Elaborazione/GestoreNotificheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreNotificheInterfaccia.java
@@ -1,11 +1,14 @@
 package Elaborazione;
 
 import java.rmi.Remote; 
-import java.rmi.RemoteException;  
+import java.rmi.RemoteException;
 
 import java.util.HashMap;
 import java.util.ArrayList;
 
 public interface GestoreNotificheInterfaccia extends Remote 
-{  
-} 
+{
+    ArrayList<HashMap<String, Object>> ricercaNotifiche(String tipoUtente, String myDateObj) throws RemoteException;
+}
+
+
