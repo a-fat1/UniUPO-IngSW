0dd413f2ee35603781f37d5718b20301e4479c2a
Diego Galletti <20044195@studenti.uniupo.it>
2023-12-19 10:36:10 +0000

    Replace UiNotificaInterfaccia.java

--- codice/src/main/java/UserInterface/UiNotificaInterfaccia.java
+++ codice/src/main/java/UserInterface/UiNotificaInterfaccia.java
@@ -5,7 +5,7 @@ import java.rmi.RemoteException;
 
 public interface UiNotificaInterfaccia extends Remote
 {
-	void avvioVisualizzaNotifiche() throws RemoteException;
+	void avvioVisualizzaNotifiche(String tipoUtente) throws RemoteException;
 	void avvioGeneraNotifica() throws RemoteException;
 	void avvioRicercaNotifiche() throws RemoteException;
 }
