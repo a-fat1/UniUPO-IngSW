7201027dc25586edff51fd165efa9db665062db3
Raffaele Enea Maria Camera <20033260@studenti.uniupo.it>
2023-12-19 12:55:43 +0000

    Replace GestoreRicercheInterfaccia.java

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -8,25 +8,8 @@ import java.util.ArrayList;
 
 public interface GestoreRicercheInterfaccia extends Remote 
 {
-    //RF11: lista ordini
-    //autore: Marino & Vecchio
-    public ArrayList<HashMap<String, Object>> ricercaPerUtente(String username) throws RemoteException;
-    public ArrayList<HashMap<String, Object>> ricercaPerProdotto(int codiceProdotto) throws RemoteException;
-    public int controlloParametri(String dataInizio, String dataFine) throws RemoteException;
-    public ArrayList<HashMap<String, Object>> ricercaListaPagamentiData(String dataInizio, String dataFine) throws RemoteException;
 
-    // RF18 Prodotti in esaurimento
-    // Alessandro Fatone, Dario Guidotti
-    int controlloGiacenza(String giacenza) throws RemoteException;
-	ArrayList<HashMap<String, Object>> ricercaProdotti(String giacenza) throws RemoteException;
+	boolean controllaParametri(String titolo, String autore, String editore, String anno, String tipo);
 
-    // RF19 RicercaUtente
-    // Nazzari Riccardo, Andrea Benedetto
-    int controlloParametriRicercaUtente(String username) throws RemoteException;
-    int controlloParametriRicercaUtente(String nome, String cognome) throws RemoteException;
-    ArrayList<HashMap<String, Object>> cercaUtenteBloccatoNonBloccato(String username) throws RemoteException;
-    ArrayList<HashMap<String, Object>> cercaUtenteNonBloccato(String username) throws RemoteException;
-    ArrayList<HashMap<String, Object>> cercaUtenteBloccatoNonBloccato(String nome, String cognome) throws RemoteException;
-    ArrayList<HashMap<String, Object>> cercaUtenteNonBloccato(String nome, String cognome) throws RemoteException;
+	ArrayList<HashMap<String, Object>> ricercaProdotto(String titolo, String autore, String editore, String anno, String tipo) throws RemoteException;  
 } 
-
