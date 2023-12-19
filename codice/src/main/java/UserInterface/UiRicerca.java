a018f68118652fef07e41a44b976aa314e804c03
Raffaele Enea Maria Camera <20033260@studenti.uniupo.it>
2023-12-19 12:56:33 +0000

    Replace UiRicerca.java

--- codice/src/main/java/UserInterface/UiRicerca.java
+++ codice/src/main/java/UserInterface/UiRicerca.java
@@ -27,6 +27,11 @@ public class UiRicerca extends JOptionPane implements UiRicercaInterfaccia
 	private GestoreRicercheInterfaccia gestoreRicerche;
 
 	// attributi
+	private String titolo;
+	private String autore;
+	private String editore;
+	private int anno;
+	private String tipo;
 	
 	// elementi grafici
 	
@@ -42,9 +47,52 @@ public class UiRicerca extends JOptionPane implements UiRicercaInterfaccia
 		gestoreRicerche = (GestoreRicercheInterfaccia) registryGestore.lookup("gestoreRicerche");
 	}
 	
-	public void avvioRicercaProdotto() throws RemoteException
-	{	// RF08
+	/*public void avvioRicercaProdotto() throws RemoteException
+	{	
+		// RF08	
+		registryUI = LocateRegistry.getRegistry("127.0.0.1", 1100); // default: 1099
+		registryGestore = LocateRegistry.getRegistry(hostGestore, 1099); 
+
+		uiUtente = (UiUtenteInterfaccia) registryUI.lookup("uiUtente");
+		gestoreRicerche = (GestoreRicercheInterfaccia) registryGestore.lookup("gestoreRicerche");
+		titolo= "", autore="", editore="", anno="", tipo="";
+		boolean esito = gestoreRicerche.controllaParametri(titolo, autore, editore, anno, tipo);
+		ArrayList<String> risultato = null;
+		if(esito){
+			risultato = gestoreRicerche.cercaProdotto( titolo, autore);
+			for(String elemento : risultato){
+				System.out.println(elemento);
+			}
+		} else {
+			System.out.println("Non hai inserito tutti i parametri obbligatori");
+		}
+	}*/
+	
+	public void avvioRicercaProdotto() throws RemoteException {
+	    // Dichiarazione e inizializzazione delle variabili
+	    String titolo = "Il tuo titolo";
+	    String autore = "Il tuo autore";
+	    String editore = "Il tuo editore";
+	    String anno = "Il tuo anno";  // Assicurati che il tipo sia corretto (Integer o String)
+	    String tipo = "Il tuo tipo";
+	    String hostGestore = "Indirizzo IP del tuo host";  // Sostituisci con l'indirizzo IP corretto
+
+	    try {
+	        // Inizializzazione dei registri
+	        registryUI = LocateRegistry.getRegistry("127.0.0.1", 1100); // default: 1099
+	        registryGestore = LocateRegistry.getRegistry(hostGestore, 1099);
+
+	        // Inizializzazione delle interfacce
+	        uiUtente = (UiUtenteInterfaccia) registryUI.lookup("uiUtente");
+	        gestoreRicerche = (GestoreRicercheInterfaccia) registryGestore.lookup("gestoreRicerche");
+	        ArrayList<HashMap<String, Object>> risultato = gestoreRicerche.ricercaProdotto(titolo, autore, editore, anno, tipo);
+	        //mostrare i risuiltati ottenuti nella finestra java swing..
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
 	}
+
 
 	public void avvioProdottiInEsaurimento() throws RemoteException
 	{	// RF18	
