174d8824534bec2adf8c091eec5b6358732723b6
Andrea Benedetto <20044519@studenti.uniupo.it>
2023-12-19 16:33:54 +0000

    Replace UiRicerca.java

--- codice/src/main/java/UserInterface/UiRicerca.java
+++ codice/src/main/java/UserInterface/UiRicerca.java
@@ -1,5 +1,6 @@
 package UserInterface;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.ArrayList;
 
@@ -29,7 +30,17 @@ public class UiRicerca extends JOptionPane implements UiRicercaInterfaccia
 	// attributi
 	
 	// elementi grafici
-	
+	// RF19
+	private JPanel searchPanel;
+	private JLabel labelUsername;
+	private JLabel labelNome;
+	private JLabel labelCognome;
+	private JComboBox comboMenu;
+	private BoxLayout boxLayout;
+	private JTextField fieldUsername;
+	private JTextField fieldNome;
+	private JTextField fieldCognome;
+	private String pulsanti[];
 	public UiRicerca(String hostGestore) throws RemoteException, NotBoundException
 	{
 		registryUI = LocateRegistry.getRegistry("127.0.0.1", 1100); // default: 1099
@@ -51,7 +62,31 @@ public class UiRicerca extends JOptionPane implements UiRicercaInterfaccia
 	}
 
 	public void avvioRicercaUtente() throws RemoteException
-	{	// RF19	
+	{	// RF19
+		// definisco gli elementi da caricare nel riquadro
+		labelUsername = new JLabel("Inserisci uno username");
+		labelNome = new JLabel("Inserisci un nome");
+		labelCognome = new JLabel("Inserisci un cognome");
+		fieldUsername = new JTextField("", 10);
+		fieldNome = new JTextField("", 10);
+		fieldCognome = new JTextField("", 10);
+		comboMenu = new JComboBox<String>();
+		comboMenu.addItem("Ricerca per nome-cognome");
+		comboMenu.addItem("Ricerca per username");
+		pulsanti[0] = "Invia";
+		setBounds(100, 100, 450, 300);
+		searchPanel = new JPanel();
+		boxLayout = new BoxLayout(searchPanel, BoxLayout.PAGE_AXIS);
+		searchPanel.setLayout(boxLayout);
+
+		searchPanel.add(comboMenu);
+		searchPanel.add(labelNome);
+		searchPanel.add(fieldNome);
+		searchPanel.add(labelCognome);
+		searchPanel.add(fieldCognome);
+		searchPanel.add(labelUsername);
+		searchPanel.add(fieldUsername);
+		JOptionPane.showOptionDialog(null, searchPanel, "Ricerca utente (clicca X per uscire)", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, pulsanti, "Registrati");
 	}
 
   	public void avvioProdottiPiuVenduti() throws RemoteException
