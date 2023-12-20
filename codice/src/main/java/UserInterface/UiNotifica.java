555989e0613e3c7ea7bf691757802d23e602edbf
Diego Galletti <20044195@studenti.uniupo.it>
2023-12-20 13:48:17 +0100

    Introdotta UI e completato RF01

--- codice/src/main/java/UserInterface/UiNotifica.java
+++ codice/src/main/java/UserInterface/UiNotifica.java
@@ -73,33 +73,62 @@ public class UiNotifica extends JOptionPane implements UiNotificaInterfaccia
 
 
 	}
-	
+
+	// RF01 Galletti-Calcaterra
+	JPanel pannello = new JPanel(); // Nuova finestra
+	JScrollPane pannello_tabella;
+
 	public void avvioVisualizzaNotifiche(String tipoUtente) throws RemoteException
-	{ 	// RF01
+	{ // RF01 Galletti-Calcaterra
 		int id;
-		String testo, tipodiUtente,dataPubblicazione,dataScadenza;
-		ArrayList<HashMap<String,Object>> lista=null;
+		String testo, tipodiUtente, dataPubblicazione, dataScadenza;
+		ArrayList<HashMap<String, Object>> lista = null;
 		LocalDateTime myDateObj = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 		// Formattare la data utilizzando il formato personalizzato
 		String formattedDate = myDateObj.format(formatter);
-		lista=gestoreNotifiche.ricercaNotifiche(tipoUtente,formattedDate);
-		int dim=lista.size();
-		if(dim==0)
-			System.out.println("\nNessuna notifica!\n");
-		else{
-			for (int i = 0; i < dim; i++) {
-				id = (int) lista.get(i).get("numero");
-				testo = (String) lista.get(i).get("testo");
-				dataPubblicazione = (String) lista.get(i).get("dataPubblicazione");
-				dataScadenza = (String) lista.get(i).get("dataScadenza");
-				tipodiUtente = (String) lista.get(i).get("tipoUtente");
-			}
-			System.out.println(dim);
+		lista = gestoreNotifiche.ricercaNotifiche(tipoUtente, formattedDate);
+		int dim = lista.size();
+		if (dim == 0) {
+			JLabel messaggio = new JLabel("Nessuna notifica");
+			pannello.add(messaggio);
+			this.showMessageDialog(pannello, messaggio);
+		} else {
+			JTable tabella = creaTabella(dim);
+			tabella=inserisciDatiTabella(tabella, lista, dim);
+			pannello_tabella = new JScrollPane(tabella);
+			pannello_tabella.setPreferredSize(new Dimension(800, 250));
+			this.showMessageDialog(null, pannello_tabella);
 		}
-
 	}
+	private JTable creaTabella(int dim) { // RF01 Galletti-Calcaterra
+		JTable tabella = new JTable(dim, 4);
+		tabella.getColumnModel().getColumn(0).setHeaderValue("Data");
+		tabella.getColumnModel().getColumn(0).setPreferredWidth(150);
+		tabella.getColumnModel().getColumn(1).setHeaderValue("Contenuto");
+		tabella.getColumnModel().getColumn(1).setPreferredWidth(400);
+		tabella.getColumnModel().getColumn(2).setHeaderValue("Destinatario");
+		tabella.getColumnModel().getColumn(2).setPreferredWidth(100);
+		tabella.getColumnModel().getColumn(3).setHeaderValue("Scadenza");
+		tabella.getColumnModel().getColumn(3).setPreferredWidth(150);
+		tabella.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		return tabella;
+	}
+	// RF01 Galletti-Calcaterra
+	private JTable inserisciDatiTabella(JTable tabella, ArrayList<HashMap<String, Object>> lista, int dim) {
+		for (int i = 0; i < dim; i++) {
+			String testo = (String) lista.get(i).get("testo");
+			String dataPubblicazione = (String) lista.get(i).get("dataPubblicazione");
+			String dataScadenza = (String) lista.get(i).get("dataScadenza");
+			String tipodiUtente = (String) lista.get(i).get("tipoUtente");
 
+			tabella.setValueAt(dataPubblicazione, i, 0);
+			tabella.setValueAt(testo, i, 1);
+			tabella.setValueAt(tipodiUtente, i, 2);
+			tabella.setValueAt(dataScadenza, i, 3);
+		}
+		return tabella;
+	}
 	/**
 	 * RF04: Avvia la generazione di una notifica
 	 *
