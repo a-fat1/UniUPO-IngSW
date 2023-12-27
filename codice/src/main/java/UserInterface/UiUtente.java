bb064df4d2a52ba6343b5648277651e52f523156
Riccardo Giovanni Gualiumi <20047331@studenti.uniupo.it>
2023-12-27 12:11:25 +0100

    initial implementation

--- codice/src/main/java/UserInterface/UiUtente.java
+++ codice/src/main/java/UserInterface/UiUtente.java
@@ -28,12 +28,18 @@ public class UiUtente extends JOptionPane implements UiUtenteInterfaccia
 	private GestoreAccessiInterfaccia gestoreAccessi;
 
 	// attributi
+	//RF02
+	private String nome;
+	private String cognome;
+	private boolean annullamentoRichiesta;
+	private int esitoControllo;
 	
 	// elementi grafici
 	//RF02
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JPanel contentPaneError;
+	private JTextField textFieldCognome;
+	private JTextField textFieldNome;
 	private String pulsantiRegistrazione[];
 	
 	public UiUtente(String hostGestore) throws RemoteException, NotBoundException
@@ -47,7 +53,14 @@ public class UiUtente extends JOptionPane implements UiUtenteInterfaccia
 	
 	public void avvioCreaUtente() throws RemoteException
 	{ 	// RF02
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		do
+		{
+			mostraFormRegistrazione();
+		}while (!annullamentoRichiesta && esitoControllo!=0);
+	}
+	
+	private void mostraFormRegistrazione() throws RemoteException {
+		
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel(new GridLayout(0, 2, 50, 5));
 		
@@ -55,22 +68,76 @@ public class UiUtente extends JOptionPane implements UiUtenteInterfaccia
 		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
 		contentPane.add(lblNewLabel);
 		
-		textField_1 = new JTextField();
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		textFieldNome = new JTextField();
+		contentPane.add(textFieldNome);
+		textFieldNome.setColumns(10);
 		
 		JLabel lblCognome = new JLabel("Cognome");
 		lblCognome.setHorizontalAlignment(SwingConstants.RIGHT);
 		contentPane.add(lblCognome);
 		
-		textField = new JTextField();
-		contentPane.add(textField);
-		textField.setColumns(10);
+		textFieldCognome = new JTextField();
+		contentPane.add(textFieldCognome);
+		textFieldCognome.setColumns(10);
 		pulsantiRegistrazione = new String[2];
 		pulsantiRegistrazione[0] = "Cancella";
 		pulsantiRegistrazione[1] = "Avanti";
 		
-		JOptionPane.showOptionDialog(null, contentPane, "Registrazione (clicca su X o cancella per uscire)", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, pulsantiRegistrazione, "Registrati");
+		int scelta = JOptionPane.showOptionDialog(null, contentPane, "Registrazione (clicca su X o cancella per uscire)", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, pulsantiRegistrazione, "Registrati");
+		
+		if (scelta == 0)
+		{
+			annullamentoRichiesta = true;	//utente vuole quittare
+			return;
+		}
+		else if (scelta == 1)
+		{
+			nome = textFieldNome.getText();
+			cognome = textFieldCognome.getText();
+			
+			esitoControllo = gestoreAccessi.controlloFormatoNomeCognome(nome, cognome);
+			if (esitoControllo == 1 || esitoControllo == 2)
+			{
+				mostraErrore(esitoControllo);
+			}
+			else if (esitoControllo == 0)
+			{
+				gestoreAccessi.promptSalvaAccount(nome, cognome);
+			}
+		}
+	}
+	
+	private void mostraErrore(int esitoControllo)
+	{
+		
+		if (esitoControllo == 1)	//errore nome
+		{
+			//creazione di contentPaneError
+			setBounds(100, 100, 450, 300);
+			contentPaneError = new JPanel();
+			contentPaneError.setBorder(new EmptyBorder(5, 5, 5, 5));
+			contentPaneError.setLayout(new GridLayout(0, 1, 50, 50));
+			
+			JLabel lblNewLabel = new JLabel("format nome errato: troppi pochi caratteri (min:3) oppure il nome contiene un numero");
+			contentPaneError.add(lblNewLabel);
+			
+			//visualizzazione contentPaneError
+			JOptionPane.showConfirmDialog(null, contentPaneError, "errore nome (premi X o OK)", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null);
+		}
+		else if (esitoControllo == 2)
+		{
+			//creazione di contentPaneError
+			setBounds(100, 100, 450, 300);
+			contentPaneError = new JPanel();
+			contentPaneError.setBorder(new EmptyBorder(5, 5, 5, 5));
+			contentPaneError.setLayout(new GridLayout(0, 1, 50, 50));
+			
+			JLabel lblNewLabel = new JLabel("format cognome errato: troppi pochi caratteri (min:3) oppure il cognome contiene un numero");
+			contentPaneError.add(lblNewLabel);
+			
+			//visualizzazione contentPaneError
+			JOptionPane.showConfirmDialog(null, contentPaneError, "errore cognome (premi X o OK)", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null);
+		}
 	}
 
 	public void avvioBloccaSbloccaUtente() throws RemoteException
