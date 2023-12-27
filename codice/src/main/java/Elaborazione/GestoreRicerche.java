5bf900045f7d67f6d85e0c3ada54093897cf54bb
Alessandro Fatone <20019780@studenti.uniupo.it>
2023-12-27 23:57:31 +0000

    Modifiche metodi elaborazione per RF18

--- codice/src/main/java/Elaborazione/GestoreRicerche.java
+++ codice/src/main/java/Elaborazione/GestoreRicerche.java
@@ -164,32 +164,38 @@ public class GestoreRicerche implements GestoreRicercheInterfaccia
 		return listaPagamenti;
 	}
 
-	@Override
 	public int controlloGiacenza(String giacenza) throws RemoteException {
 
-		// RF18 Prodotti in esaurimento
+	    // RF18: Prodotti in esaurimento
 		// Alessandro Fatone, Dario Guidotti
 
-		if (giacenza == null) 
+		if (giacenza == null || !giacenza.matches("\\d+"))	// Controllo per giacenza nulla e caratteri numerici
 			return 1;
-		if (!giacenza.matches("\\d+"))
+
+		String valoreMassimoIntero = String.valueOf(Integer.MAX_VALUE);		// Controllo lunghezza giacenza con Integer.MAX_VALUE
+		if (giacenza.length() > valoreMassimoIntero.length())
 			return 2;
-		if (Integer.parseInt(giacenza) == 0)
+		else
+			if(giacenza.length() == valoreMassimoIntero.length())	// Quando hanno la stessa lunghezza
+				for (int i = 0; i < valoreMassimoIntero.length(); i++)	// Controllo giacenza con Integer.MAX_VALUE per ogni carattere
+					if(giacenza.charAt(i) > valoreMassimoIntero.charAt(i))
+						return 2;
+
+		if (Integer.parseInt(giacenza) == 0)	// Controllo giacenza diversa da zero
 			return 3;
 
 		return 0;
 	}
 	
-	@Override
 	public ArrayList<HashMap<String, Object>> ricercaProdotti(String giacenza) throws RemoteException {
-		
-		// RF18 Prodotti in esaurimento
-		// Alessandro Fatone, Dario Guidotti
 
-		try {
+        // RF18: Prodotti in esaurimento
+		// Alessandro Fatone, Dario Guidotti
+		
+		try {	// Se la query ha successo ritorna un ArrayList con il contenuto desiderato
 			return dbProdotti.query("SELECT * FROM Prodotto WHERE Quantita <= " + giacenza);
 		} catch (RemoteException e) {
-			return new ArrayList<>();
+			return new ArrayList<>();	// In caso contrario viene ritornato un ArrayList vuoto
 		}
     }
 
