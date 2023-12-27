010b0df4ef998c06858f3d18dc68b4ddd9c27558
Riccardo Giovanni Gualiumi <20047331@studenti.uniupo.it>
2023-12-27 15:30:23 +0100

    Aggiunta commenti in UiUtenteInterfaccia.java

--- codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
+++ codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
@@ -5,6 +5,7 @@ import java.rmi.RemoteException;
 
 public interface UiUtenteInterfaccia extends Remote
 {
+	//RF02
 	void avvioCreaUtente(boolean cliente) throws RemoteException;
 	void avvioBloccaSbloccaUtente() throws RemoteException;
 	void avvioAggiornaDomicilio() throws RemoteException;
