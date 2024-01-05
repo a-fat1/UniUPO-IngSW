e14a0fa056572190f57b10b3e95f331c88268ec5
Alessandro Fatone <20019780@studenti.uniupo.it>
2024-01-05 23:04:03 +0000

    Modifica nomi metodi

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -1,12 +1,12 @@
 package Elaborazione;
 
-import java.rmi.Remote;
-import java.rmi.RemoteException;
+import java.rmi.Remote; 
+import java.rmi.RemoteException;  
 
 import java.util.HashMap;
 import java.util.ArrayList;
 
 public interface GestoreRicercheInterfaccia extends Remote {
     int controlloGiacenza(String giacenza) throws RemoteException;
-	ArrayList<HashMap<String, Object>> ricercaProdotti(String giacenza) throws RemoteException;
+	ArrayList<HashMap<String, Object>> ricercaProdottiConGiacenza(String giacenza) throws RemoteException;
 }
