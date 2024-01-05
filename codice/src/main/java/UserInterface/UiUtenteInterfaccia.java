375f6eb2c76b8a055d7f3f77d029d3a85f676ffd
Riccardo Giovanni Gualiumi <20047331@studenti.uniupo.it>
2024-01-05 16:37:46 +0100

    Patch bugs relativi alla merge

--- codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
+++ codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
@@ -7,10 +7,7 @@ import java.rmi.NotBoundException;
 
 public interface UiUtenteInterfaccia extends Remote
 {
-<<<<<<< HEAD
-=======
 	//RF02
->>>>>>> c5c81cbeb1a340c9b454f92c7fb0e16de334b2dc
 	void avvioCreaUtente(boolean cliente) throws RemoteException, NotBoundException;
 	void avvioBloccaSbloccaUtente() throws RemoteException;
 	void avvioAggiornaDomicilio() throws RemoteException;
