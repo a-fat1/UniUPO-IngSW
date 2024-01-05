b7ad48f5a1c1d37e71d0394355cb81f09c5ed8d2
Riccardo Giovanni Gualiumi <20047331@studenti.uniupo.it>
2024-01-05 16:55:45 +0100

    aggiunta commento in UserInterface

--- codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
+++ codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
@@ -7,6 +7,7 @@ import java.rmi.NotBoundException;
 
 public interface UiUtenteInterfaccia extends Remote
 {
+	//RF02
 	void avvioCreaUtente(boolean cliente) throws RemoteException, NotBoundException;
 	void avvioBloccaSbloccaUtente() throws RemoteException;
 	void avvioAggiornaDomicilio() throws RemoteException;
