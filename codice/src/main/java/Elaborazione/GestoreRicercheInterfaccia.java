08309731f40f88ef0a2763f2109b50cd515422e4
Alessandro Fatone <20019780@studenti.uniupo.it>
2024-01-06 14:00:43 +0000

    Update GestoreRicercheInterfaccia.java

--- codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreRicercheInterfaccia.java
@@ -1,7 +1,7 @@
 package Elaborazione;
 
-import java.rmi.Remote; 
-import java.rmi.RemoteException;  
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 import java.util.HashMap;
 import java.util.ArrayList;
