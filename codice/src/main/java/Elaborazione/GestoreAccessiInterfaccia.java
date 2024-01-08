e4011c7f7bc1f92ba80bf84dc8cb8015a60acded
Marco Brivio <20047184@studenti.uniupo.it>
2024-01-08 15:55:11 +0000

    Replace GestoreAccessiInterfaccia.java

--- codice/src/main/java/Elaborazione/GestoreAccessiInterfaccia.java
+++ codice/src/main/java/Elaborazione/GestoreAccessiInterfaccia.java
@@ -4,20 +4,24 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;  
 
 import java.util.HashMap;
-import java.util.ArrayList;
 
 public interface GestoreAccessiInterfaccia extends Remote 
 {  
    String controllaFormatoCredenziali(String username, String password) throws RemoteException;
    String ricercaCredenziali(String username, String password) throws RemoteException;
    HashMap<String, Object> ricercaUtente(String username) throws RemoteException;
+   int verificaCredenziali(String password, String passwordAttuale) throws RemoteException;
+   int controlloNuovaPassword(String nuovaPassword) throws RemoteException;
+   void AggiornaPassword(String username, String nuovaPassword) throws RemoteException;
+   
+   //RF02
+   int controlloFormatoNomeCognome(String nome, String cognome)throws RemoteException;
+   void promptSalvaAccount(String nome, String cognome) throws RemoteException;
+   void richiestaAttivazioneAccount(String nome, String cognome, String tipoUtente) throws RemoteException;
+   void aggiuntaCredenziali(String username) throws RemoteException;
+
+   //RF23
    boolean verificaDuplicato(String nuovoUsername) throws RemoteException;
    int verifica(String vecchioUsername, String nuovoUsername) throws RemoteException;
    void cambio(String vecchioUsername, String nuovoUsername) throws RemoteException;
-   void mostraErrore(int val) throws RemoteException;
-   void messaggio(String messaggio) throws RemoteException;
 } 
-
-
-
-
