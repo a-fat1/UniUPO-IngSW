fbae6871128fa4e4515ef23feb1fd363f8314224
Marco Brivio <20047184@studenti.uniupo.it>
2024-01-08 16:09:28 +0000

    Replace UiLoginInterfaccia.java

--- codice/src/main/java/UserInterface/UiLoginInterfaccia.java
+++ codice/src/main/java/UserInterface/UiLoginInterfaccia.java
@@ -1,12 +1,14 @@
 package UserInterface;
 
-import java.rmi.Remote; 
+import java.rmi.NotBoundException;
+import java.rmi.Remote;
 import java.rmi.RemoteException; 
+import java.rmi.NotBoundException;
 
 public interface UiLoginInterfaccia extends Remote
 {
-	 void avvioLogin() throws RemoteException;
-	 void avvioAggiornaUsername() throws RemoteException;
-	 void avvioAggiornaPassword() throws RemoteException;
+	 void avvioLogin() throws RemoteException, NotBoundException;
+	 void avvioAggiornaUsername(String vecchioUsername) throws RemoteException;
+	 void avvioAggiornaPassword(Boolean loggato, String username, String Password) throws RemoteException;
 }
 
