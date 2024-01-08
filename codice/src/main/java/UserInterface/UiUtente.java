1b6e8ec4cd61c38b7216f334abd2b3243351e4aa
Federico Mondelli <20044357@studenti.uniupo.it>
2024-01-08 21:17:29 +0100

    Fix UIUtente

--- codice/src/main/java/UserInterface/UiUtente.java
+++ codice/src/main/java/UserInterface/UiUtente.java
@@ -80,6 +80,40 @@ public class UiUtente extends JOptionPane implements UiUtenteInterfaccia
 	}
 
 	public void avvioAggiornaDomicilio(String username, boolean nuovo) throws RemoteException {
+		int ckFormato = 0;
+		int scelta = 0;
+
+		do {
+			scelta = mostraFormDomicilio(nuovo);
+
+			if (scelta == 1) {
+				textField_via.setBackground(Color.WHITE);
+				textField_cap.setBackground(Color.WHITE);
+				textField_localita.setBackground(Color.WHITE);
+				textField_civico.setBackground(Color.WHITE);
+
+				// Recupera i valori inseriti dall'utente
+				String via = textField_via.getText();
+				String civico = textField_civico.getText();
+				String cap = textField_cap.getText();
+				String localita = textField_localita.getText();
+
+				// Controlla se ci sono errori di formato
+				ckFormato = gestoreAccessi.controllaFormatoDomicilio(via, civico, cap, localita);
+				if (ckFormato > 0) {
+					this.mostraErroreFormatoDomicilio(ckFormato);
+				} else {
+					// Chiamata al metodo per aggiornare il domicilio
+					gestoreAccessi.promptSalvaDomicilio(username, via, civico, cap, localita);
+					this.mostraSuccessoAggiornamentoDomicilio();
+				}
+			}
+		} while(ckFormato > 0 && scelta == 1);
+	}
+
+	private int mostraFormDomicilio(boolean nuovo) {
+		int scelta = 0;
+
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel(new GridLayout(0, 2, 50, 5));
 
@@ -119,64 +153,37 @@ public class UiUtente extends JOptionPane implements UiUtenteInterfaccia
 		pulsantiRegistrazione[0] = "Indietro";
 		pulsantiRegistrazione[1] = "Conferma";
 
-		int ckFormato = 0;
-		int scelta = 0;
+		// Controlla se il metodo e' stato invocato da RF02 (creazione utente)
+		if (nuovo) {
+			scelta = JOptionPane.showOptionDialog(null,
+					contentPane,
+					"Aggiornamento Domicilio (clicca su X per uscire)",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
+					null,
+					new Object[]{pulsantiRegistrazione[1]},
+					"Aggiorna");
+		}
+		else { // Il metodo è invocato dal menu' del client
+			String[] partiDomicilio = gestoreAccessi.promptRecuperaDomicilio(username);
 
-		do {
-			// Controlla se il metodo e' stato invocato da RF02 (creazione utente)
-			if (nuovo) {
-
-				scelta = JOptionPane.showOptionDialog(null,
-						contentPane,
-						"Aggiornamento Domicilio (clicca su X per uscire)",
-						JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
-						null,
-						new Object[]{pulsantiRegistrazione[1]},
-						"Aggiorna");
-			}
-			else { // Il metodo è invocato dal menu' del client
-				String[] partiDomicilio = gestoreAccessi.promptRecuperaDomicilio(username);
-
-				if (partiDomicilio.length == 4) {
-					System.out.println("4");
-					textField_via.setText(partiDomicilio[0]);
-					textField_civico.setText(partiDomicilio[1]);
-					textField_cap.setText(partiDomicilio[2]);
-					textField_localita.setText(partiDomicilio[3]);
-				}
-
-				scelta = JOptionPane.showOptionDialog(null,
-						contentPane,
-						"Aggiornamento Domicilio (clicca su X o Indietro per uscire)",
-						JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
-						null,
-						pulsantiRegistrazione,
-						"Aggiorna");
+			if (partiDomicilio.length == 4) {
+				System.out.println("4");
+				textField_via.setText(partiDomicilio[0]);
+				textField_civico.setText(partiDomicilio[1]);
+				textField_cap.setText(partiDomicilio[2]);
+				textField_localita.setText(partiDomicilio[3]);
 			}
 
-			if (scelta == 1) {
-				textField_via.setBackground(Color.WHITE);
-				textField_cap.setBackground(Color.WHITE);
-				textField_localita.setBackground(Color.WHITE);
-				textField_civico.setBackground(Color.WHITE);
+			scelta = JOptionPane.showOptionDialog(null,
+					contentPane,
+					"Aggiornamento Domicilio (clicca su X o Indietro per uscire)",
+					JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE,
+					null,
+					pulsantiRegistrazione,
+					"Aggiorna");
+		}
 
-				// Recupera i valori inseriti dall'utente
-				String via = textField_via.getText();
-				String civico = textField_civico.getText();
-				String cap = textField_cap.getText();
-				String localita = textField_localita.getText();
-
-				// Controlla se ci sono errori di formato
-				ckFormato = gestoreAccessi.controllaFormatoDomicilio(via, civico, cap, localita);
-				if (ckFormato > 0) {
-					this.mostraErroreFormatoDomicilio(ckFormato);
-				} else {
-					// Chiamata al metodo per aggiornare il domicilio
-					gestoreAccessi.promptSalvaDomicilio(username, via, civico, cap, localita);
-					this.mostraSuccessoAggiornamentoDomicilio();
-				}
-			}
-		} while(ckFormato > 0 && scelta == 1);
+		return scelta;
 	}
 
 	private void mostraErroreFormatoDomicilio(int msg) {
