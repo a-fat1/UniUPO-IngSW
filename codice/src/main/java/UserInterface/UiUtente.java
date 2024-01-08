5a18a33c8cd4047cb6846dffe45b6eccc1093523
Gianluca Veronese <gianlucaveronese@MacBook-Air-di-Gianluca.local>
2024-01-08 15:46:34 +0100

    commit

--- codice/src/main/java/UserInterface/UiUtente.java
+++ codice/src/main/java/UserInterface/UiUtente.java
@@ -3,8 +3,8 @@ package UserInterface;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-import java.rmi.registry.Registry; 
-import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
 import java.rmi.RemoteException;
 import java.rmi.NotBoundException;
 
@@ -22,27 +22,74 @@ public class UiUtente extends JOptionPane implements UiUtenteInterfaccia
 	// componenti
 	private UiNotificaInterfaccia uiNotifica;
 	private GestoreAccessiInterfaccia gestoreAccessi;
+	
 
 	// attributi
-	
+	private int selezione;
 	// elementi grafici
-	
+
 	public UiUtente(String hostGestore) throws RemoteException, NotBoundException
 	{
 		registryUI = LocateRegistry.getRegistry("127.0.0.1", 1100); // default: 1099
-		registryGestore = LocateRegistry.getRegistry(hostGestore, 1099); 
+		registryGestore = LocateRegistry.getRegistry(hostGestore, 1099);
 
 		uiNotifica = (UiNotificaInterfaccia) registryUI.lookup("uiNotifica");
 		gestoreAccessi = (GestoreAccessiInterfaccia) registryGestore.lookup("gestoreAccessi");
 	}
-	
+
 	public void avvioCreaUtente() throws RemoteException
 	{ 	// RF02
 	}
 
-	public void avvioBloccaSbloccaUtente() throws RemoteException
-	{ 	// RF20	
+	public void avvioBloccaSbloccaUtente(String username, boolean attivo) throws RemoteException
+	{ 	// RF20
+		if(attivo)
+		{
+			this.mostraFormBlocco();
+			if(selezione==1) {
+
+				gestoreAccessi.bloccoUtente(username);
+
+			}
+		}
+		else{
+			this.mostraFormSblocco();
+			if(selezione==1) {
+
+				gestoreAccessi.sbloccoUtente(username);
+
+			}
+		}
 	}
+
+	private void mostraFormBlocco() {
+		String[] scelte= { "ANNULLA","CONFERMA"};
+		selezione = JOptionPane.showOptionDialog(null, null, "FORM BLOCCO", 0, 2, null, scelte, null);
+
+		if (selezione == 1) {
+			JOptionPane.showMessageDialog(null, null, "Conferma lettura del messaggio(OK o X per continuare).", JOptionPane.INFORMATION_MESSAGE);
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, null, "annullamento", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+	private void mostraFormSblocco() {
+		String[] scelte= { "ANNULLA","CONFERMA"};
+		selezione = JOptionPane.showOptionDialog(null, null, "FORM SBLOCCO", 0, 3, null, scelte, null);
+
+		if (selezione == 1) {
+			JOptionPane.showMessageDialog(null, null, "Conferma lettura del messaggio(OK o X per continuare).", JOptionPane.INFORMATION_MESSAGE);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, null, "annullamento", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
+
+
+
 
 	public void avvioAggiornaDomicilio() throws RemoteException
 	{ 	// RF24
--- codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
+++ codice/src/main/java/UserInterface/UiUtenteInterfaccia.java
@@ -1,12 +1,12 @@
 package UserInterface;
 
-import java.rmi.Remote; 
-import java.rmi.RemoteException; 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface UiUtenteInterfaccia extends Remote
 {
 	void avvioCreaUtente() throws RemoteException;
-	void avvioBloccaSbloccaUtente() throws RemoteException;
+	void avvioBloccaSbloccaUtente(String username, boolean attivo) throws RemoteException;
 	void avvioAggiornaDomicilio() throws RemoteException;
+	
 }
-
