6b2ad4efec8a6cf7921b54f1c484304ad2e16d33
Luca Iacobucci <20035727@studenti.uniupo.it>
2024-01-09 17:14:40 +0000

    Aggiunta test di RF09 aggiunta al carrello

---
+++ codice/src/test/java/RF09AggiuntaAlCarrello.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import org.junit.Test;
+
+import DataBase.DbProdotti;
+import Elaborazione.GestoreCarrelli;
+
+public class testAggiungiAlCarrello {
+     
+    GestoreCarrelli gestore = new GestoreCarrelli(new DbProdotti());
+    
+    @Test
+    public void testAggiuntaAlCarrello() throws RemoteException, NotBoundException{
+
+    //controllo inserimento quantità prodotto disponibile
+    assertTrue(gestore.controlloLimiteQuantita(2));
+    
+    //controllo inserimento quantità non valida
+    assertFalse(gestore.controlloLimiteQuantita(4));
+    assertFalse(gestore.controlloLimiteQuantita(0));
+    
+    //controllo di prodotto disponibile
+    assertTrue(gestore.controlloDisponibilita(100000));
+    
+    //controllo di prodotto non disponibile
+    assertFalse(gestore.controlloDisponibilita(100003));
+
+    }
+
+}
