f4bc9cc86cba1c3161db545620f3eedfc225a5e4
Luca Iacobucci <20035727@studenti.uniupo.it>
2024-01-09 20:00:33 +0000

    rf09 aggiunto throws RemoteException a mostraErrore(int)

--- codice/src/main/java/UserInterface/UiCarrelloInterfaccia.java
+++ codice/src/main/java/UserInterface/UiCarrelloInterfaccia.java
@@ -18,7 +18,7 @@ public interface UiCarrelloInterfaccia extends Remote
 	
 	void avvioAggiungiAlCarrello(String username, int codiceProdotto) throws RemoteException;
 	public void mostraForm() throws RemoteException;
-	public void mostraErrore(int numberError);
+	public void mostraErrore(int numberError) throws RemoteException;
 
 }
 
