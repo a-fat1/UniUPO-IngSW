7f921e99eef39280288c120e79abed9e0b84230a
Luca Iacobucci <20035727@studenti.uniupo.it>
2024-01-09 16:14:47 +0000

    Update UiCarrello.java, aggiunte parti RF09 aggiunta al carrello

--- codice/src/main/java/UserInterface/UiCarrello.java
+++ codice/src/main/java/UserInterface/UiCarrello.java
@@ -10,6 +10,8 @@ import java.rmi.NotBoundException;
 
 import javax.swing.*;
 
+import java.awt.GridLayout; /*RF09*/
+
 import UserInterface.*;
 import Elaborazione.*;
 
@@ -34,6 +36,21 @@ public class UiCarrello extends JOptionPane implements UiCarrelloInterfaccia
 	private JPanel messaggioConferma;
 	private String pulsantiConferma[];
 	private String pulsanteNotifica[];
+
+
+	//RF09 : Aggiunta al carrello
+	//attributi
+	private int sceltaQuantita; 
+	private int quantita; 
+
+	//elementi grafici
+	private JLabel quantitaLabel;	/*RF09*/
+	private JTextField quantitaField; /*RF09*/
+	private JPanel formPanel;		/*RF09*/
+	private String[] pulsanteOK;	/*RF09*/
+
+
+
 	
 	public UiCarrello(String hostGestore) throws RemoteException, NotBoundException
 	{
@@ -58,6 +75,21 @@ public class UiCarrello extends JOptionPane implements UiCarrelloInterfaccia
         confermaRimozione = new JPanel();
         
         messaggioConferma = new JPanel();
+
+
+	//RF09 : aggiunta al carrello 
+	quantitaLabel = new JLabel("Quantita'");		//RF09
+	quantitaField = new JTextField("", 10); 		//RF09
+
+	formPanel = new JPanel(new GridLayout(1,2));	//RF09
+	formPanel.add(quantitaLabel);  					//RF09
+	formPanel.add(quantitaField); 					//RF09
+	
+	pulsanteOK = new String[1]; 					//RF09
+		pulsanteOK[0] = "Ok"; 						//RF09
+
+
+
 	}
 	
 	public void avvioVisualizzaCarrello() throws RemoteException
@@ -154,7 +186,75 @@ public class UiCarrello extends JOptionPane implements UiCarrelloInterfaccia
 	}
 
 	
-	public void avvioAggiungiAlCarrello() throws RemoteException
-	{	// RF09	
+	public void avvioAggiungiAlCarrello(String username, int codiceProdotto) throws RemoteException
+	{	// RF09	: Aggiunta al carrello
+		// Autori: Fasano Lorenzo, Iacobucci Luca;
+
+
+		//prendo la quantita' inserita dall'utente
+		mostraForm();
+
+		//controllo che il prodotto selezionato dall'utente sia disponibile in database
+		if(gestoreCarrelli.controlloDisponibilita(codiceProdotto)){
+			System.out.println("1)Controllo disponibilita': OK");
+		//mostra un messaggio di errore in caso di indisponibilita' del prodotto
+		}else{
+			mostraErrore(1);
+		}
+	
+		//controllo che la quantita' richiesta dall'utente sia compresa tra 1 e 3
+		if(gestoreCarrelli.controlloLimiteQuantita(quantita)){
+			System.out.println("2)Controllo limite quantita': OK");
+		//mostra errore in qualsiasi altro caso
+		}else{
+			mostraErrore(2);
+		}
+
+		//aggiorno la quantita del prodotto in database
+		gestoreCarrelli.aggiornamentoQuantita(quantita,
+		codiceProdotto,username);
+		
+		this.showMessageDialog(null, "Il carrello e' stato aggiornato", "Carrello 			aggiornato", this.PLAIN_MESSAGE, null);
+
 	}
+
+	
+		public void mostraForm() throws RemoteException
+	{	// RF09	: Aggiunta al carrello
+		// Autori: Fasano Lorenzo, Iacobucci Luca;
+
+		sceltaQuantita = this.showOptionDialog(null, formPanel, "Inserire quantita'", 		DEFAULT_OPTION, QUESTION_MESSAGE, null, pulsanteOK, "OK");
+
+		//utente preme X
+				if(sceltaQuantita == -1){
+			System.out.println("Scelta annullata.");
+			mostraErrore(sceltaQuantita);
+		}
+			//utente preme OK
+		if(sceltaQuantita == 0){
+			//aggiungere numberFormatException
+			quantita = Integer.parseInt(quantitaField.getText());
+			//controllo quantita inserita x vedere numero
+			System.out.println("Quantita' inserita:" + quantita);
+		}
+	}
+		//RF09 : Aggiunta al carrello
+		public void mostraErrore(int numberError){
+		// RF09	: Aggiunta al carrello
+		// Autori: Fasano Lorenzo, Iacobucci Luca;
+				switch (numberError) {
+					case 1:
+						this.showMessageDialog(null, "Prodotto non disponibile", "Errore disponibilita' prodotto", this.PLAIN_MESSAGE, null);
+						break;
+					
+					case 2:
+						this.showMessageDialog(null, "La quantita' deve essere compresa tra 1 e 3", "Errore quantita'", this.PLAIN_MESSAGE, null);
+						break;
+					
+					default:
+						System.out.println("Scelta annullata.");
+						break;
+				}
+	
+
 }
