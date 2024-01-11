618d65117df76bb917f7e375508ffc8ceefc9da6
Luca Paolillo <20045638@studenti.uniupo.it>
2024-01-11 10:48:36 +0000

    Aggiunte interfacce metodi RF14

--- codice/src/main/java/UserInterface/UiProdottoInterfaccia.java
+++ codice/src/main/java/UserInterface/UiProdottoInterfaccia.java
@@ -1,12 +1,13 @@
 package UserInterface;
 
 import java.rmi.Remote; 
-import java.rmi.RemoteException; 
+import java.rmi.RemoteException;
+import java.util.HashMap; 
 
 public interface UiProdottoInterfaccia extends Remote
 {
 	void avvioRimuoviRipristinaNelCatalogo() throws RemoteException;
-	void avvioAggiornaPrezzo() throws RemoteException;
+	void avvioAggiornaPrezzo(HashMap<String, Object> P) throws RemoteException;
 	void avvioNuovaFornitura() throws RemoteException;
 	void avvioNuovoProdotto() throws RemoteException;
 	void avvioIncrementaDecrementaPrezzi() throws RemoteException;
