78c1f4dd7197b187fdb2e44cffb26582defc3996
Marco Brivio <20047184@studenti.uniupo.it>
2024-01-13 09:11:42 +0000

    Update UiLoginInterfaccia.java

--- codice/src/main/java/UserInterface/UiLoginInterfaccia.java
+++ codice/src/main/java/UserInterface/UiLoginInterfaccia.java
@@ -7,7 +7,7 @@ import java.rmi.RemoteException;
 public interface UiLoginInterfaccia extends Remote
 {
 	 void avvioLogin() throws RemoteException, NotBoundException;
-	 void avvioAggiornaUsername(String vecchioUsername) throws RemoteException;
+	 String avvioAggiornaUsername(String vecchioUsername) throws RemoteException;
 	 String avvioAggiornaPassword(Boolean loggato, String username, String Password) throws RemoteException;
 }
 
