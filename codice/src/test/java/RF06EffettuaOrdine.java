f324ce7a185f8b4198e0472f8fa92db99f3a585e
Virginia Luini <20045705@studenti.uniupo.it>
2024-01-15 13:54:53 +0000

    Replace RF06EffettuaOrdine.java

--- codice/src/test/java/RF06EffettuaOrdine.java
+++ codice/src/test/java/RF06EffettuaOrdine.java
@@ -3,50 +3,75 @@ import org.junit.Test;
 
 import DataBase.DbProdotti;
 import Elaborazione.GestoreCarrelli;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class RF06EffettuaOrdine
+public class RF06EffettuaOrdine 
 {
     DbProdotti dbProdotti = new DbProdotti();
     GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdotti);
 
 
-    /*
+    
     @Test
     public void RF06_ControllaNumeroCarta1()
     {
         //errore troppi numeri
-        assertEquals("false", gestoreCarrelli.Rf06_ControllaNumeroCarta1(672813924677863895));
+        assertFalse(gestoreCarrelli.RF06_ControllaNumeroCarta1("672813924677863895"));
+        
     }
 
     @Test
     public void RF06_ControllaNumeroCarta2()
     {
         //errore pochi numeri
-        assertEquals("false", gestoreCarrelli.Rf06_ControllaNumeroCarta2(67281392467795));
+        assertFalse(gestoreCarrelli.RF06_ControllaNumeroCarta2("67281392467795"));
     }
 
     @Test
     public void RF06_ControllaNumeroCarta3()
     {
         //ok
-        assertEquals("ok", gestoreCarrelli.Rf06_ControllaNumeroCarta3(6728139246779538));
+        assertTrue(gestoreCarrelli.RF06_ControllaNumeroCarta3("6728139246779538"));
     }
-    */
+    
 
+   @Test
+    public void RF06_CalcolaPrezzoTotale() {
+        
+    	DbProdotti dbProdottiMock = new DbProdotti();
+        GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdottiMock);
+
+        System.out.println("inizio il test");
+        ArrayList<HashMap<String, Object>> carrello = new ArrayList<>();
+        HashMap<String, Object> prodotto1 = new HashMap<>();
+        prodotto1.put("prodotto123",  10.0); 
+        prodotto1.put("quantità",  2);
+        carrello.add(prodotto1);
+
+        List<String> prodotti = new ArrayList<>();
+        prodotti.add("prodotto123");
+
+      
+        float risultato = gestoreCarrelli.RF06_CalcolaPrezzoTotale(carrello, prodotti);
+      System.out.println("il risultato : "+risultato);
+        
+        assertTrue(risultato== 20.0); // Sostituisci con il risultato atteso
+    }
     /*
     @Test
     public void RF06_CalcolaPrezzoTotale1()
     {
         //prezzo errato
-        assertEquals("prezzoErrato", gestoreCarrelli.Rf06_CalcolaPrezzoTotale1(-12,00));
+        assertFalse(gestoreCarrelli.CalcolaPrezzoTotale(-12,00));
     }
 
     @Test
     public void RF06_CalcolaPrezzoTotale2()
     {
         //prezzo corretto
-        assertEquals("prezzoCorretto", gestoreCarrelli.Rf06_CalcolaPrezzoTotale2(25,50));
-    }
-    */
+        assertTrue(gestoreCarrelli.CalcolaPrezzoTotale2(25,50));
+    }*/
+    
 }
-
