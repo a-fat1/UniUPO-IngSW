fdcf5e60200a97c6f4ad063dc268d2d168f7edab
Jorelle Madeleine MENGAPTCHE YOUMBI <20047743@studenti.uniupo.it>
2024-01-17 23:08:47 +0000

    Replace RF06EffettuaOrdine.java

--- codice/src/test/java/RF06EffettuaOrdine.java
+++ codice/src/test/java/RF06EffettuaOrdine.java
@@ -17,52 +17,7 @@ public class RF06EffettuaOrdine
     GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdotti);
 
 
-    /*
-    @Test
-    public void RF06_ControllaNumeroCarta1 throws RemoteException()
-    {
-        GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(new DbProdotti());
-
-        // Test caso in cui il numero di carta sia valido
-        assertTrue(gestoreCarrelli.controllaNumeroCarta("1234567890123456"));
-
-        // Test caso in cui il numero di carta non sia valido (lunghezza diversa da 16)
-        assertFalse(gestoreCarrelli.controllaNumeroCarta("123"));
-
-        // Test caso in cui il numero di carta non sia valido (contiene caratteri non numerici)
-        assertFalse(gestoreCarrelli.controllaNumeroCarta("123456789012345a"));
-    }
-    }
-
-    @Test
-    public void RF06_ControllaNumeroCarta2()
-    {
-        //errore pochi numeri
-        assertEquals("false", gestoreCarrelli.Rf06_ControllaNumeroCarta2(67281392467795));
-    }
-
-    @Test
-    public void RF06_ControllaNumeroCarta3()
-    {
-        //ok
-        assertEquals("ok", gestoreCarrelli.Rf06_ControllaNumeroCarta3(6728139246779538));
-    }
-    */
-
-    /*
-    @Test
-    public void RF06_CalcolaPrezzoTotale1()
-    {
-        //prezzo errato
-        assertEquals("prezzoErrato", gestoreCarrelli.Rf06_CalcolaPrezzoTotale1(-12,00));
-    }
-
-    @Test
-    public void RF06_CalcolaPrezzoTotale2()
-    {
-        //prezzo corretto
-        assertEquals("prezzoCorretto", gestoreCarrelli.Rf06_CalcolaPrezzoTotale2(25,50));
-    }
+    
     */
     @Test
     public void testControllaNumeroCarta() throws RemoteException {
@@ -78,30 +33,7 @@ public class RF06EffettuaOrdine
         assertFalse(gestoreCarrelli.controllaNumeroCarta("123456789012345a"));
     }
     @Test
-    public void testCalcolaPrezzoTotale() {
-        
-    	DbProdotti dbProdottiMock = new DbProdotti(); 
-        GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdottiMock);
-
-        ArrayList<HashMap<String, Object>> carrello = new ArrayList<>();
-        HashMap<String, Object> prodotto1 = new HashMap<>();
-        prodotto1.put("prodotto123", 10.0); 
-        prodotto1.put("quantità", 2);
-        carrello.add(prodotto1);
-
-        List<String> prodotti = new ArrayList<>();
-        prodotti.add("prodotto123");
- 
-        float risultato = gestoreCarrelli.calcolaPrezzoTotale(carrello, prodotti);
-
-        boolean risult;
-        if(risultato==20)
-        	risult=true;
-        else
-        	risult= false;
-        
-        assertTrue(risult);
-    }
+  
     
     @Test
     public void testAggiornaOrdini() throws RemoteException {
