fae1ebe8d169dcbf3525e9794facb753e70ea6c5
Jorelle Madeleine MENGAPTCHE YOUMBI <20047743@studenti.uniupo.it>
2024-01-17 22:59:26 +0000

    Replace RF06EffettuaOrdine.java

--- codice/src/test/java/RF06EffettuaOrdine.java
+++ codice/src/test/java/RF06EffettuaOrdine.java
@@ -1,67 +1,129 @@
-import static org.junit.Assert.*; 
-import org.junit.Test;
-
-
-import DataBase.DbProdotti;
-import Elaborazione.GestoreCarrelli;
+import static org.junit.Assert.*;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-public class RF06EffettuaOrdine 
+import org.junit.Test;
+
+import DataBase.DbProdotti;
+import DataBase.DbProdottiInterfaccia;
+import Elaborazione.GestoreCarrelli;
+
+public class RF06EffettuaOrdine
 {
     DbProdotti dbProdotti = new DbProdotti();
     GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdotti);
 
 
-    
+    /*
     @Test
-    public void RF06_ControllaNumeroCarta1() throws RemoteException
+    public void RF06_ControllaNumeroCarta1 throws RemoteException()
     {
-        //errore troppi numeri
-        assertFalse(gestoreCarrelli.controllaNumeroCarta("672813924677863895"));
-        
+        GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(new DbProdotti());
+
+        // Test caso in cui il numero di carta sia valido
+        assertTrue(gestoreCarrelli.controllaNumeroCarta("1234567890123456"));
+
+        // Test caso in cui il numero di carta non sia valido (lunghezza diversa da 16)
+        assertFalse(gestoreCarrelli.controllaNumeroCarta("123"));
+
+        // Test caso in cui il numero di carta non sia valido (contiene caratteri non numerici)
+        assertFalse(gestoreCarrelli.controllaNumeroCarta("123456789012345a"));
+    }
     }
 
     @Test
-    public void RF06_ControllaNumeroCarta2() throws RemoteException
+    public void RF06_ControllaNumeroCarta2()
     {
         //errore pochi numeri
-        assertFalse(gestoreCarrelli.controllaNumeroCarta("67281392467795"));
+        assertEquals("false", gestoreCarrelli.Rf06_ControllaNumeroCarta2(67281392467795));
     }
 
     @Test
-    public void RF06_ControllaNumeroCarta3() throws RemoteException
+    public void RF06_ControllaNumeroCarta3()
     {
         //ok
-        assertTrue(gestoreCarrelli.controllaNumeroCarta("6728139246779538"));
+        assertEquals("ok", gestoreCarrelli.Rf06_ControllaNumeroCarta3(6728139246779538));
     }
-    
+    */
 
-   @Test
-    public void testCalcolaPrezzoTotale() throws RemoteException {
+    /*
+    @Test
+    public void RF06_CalcolaPrezzoTotale1()
+    {
+        //prezzo errato
+        assertEquals("prezzoErrato", gestoreCarrelli.Rf06_CalcolaPrezzoTotale1(-12,00));
+    }
+
+    @Test
+    public void RF06_CalcolaPrezzoTotale2()
+    {
+        //prezzo corretto
+        assertEquals("prezzoCorretto", gestoreCarrelli.Rf06_CalcolaPrezzoTotale2(25,50));
+    }
+    */
+    @Test
+    public void testControllaNumeroCarta() throws RemoteException {
+        GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(new DbProdotti());
+
+        // Test caso in cui il numero di carta sia valido
+        assertTrue(gestoreCarrelli.controllaNumeroCarta("1234567890123456"));
+
+        // Test caso in cui il numero di carta non sia valido (lunghezza diversa da 16)
+        assertFalse(gestoreCarrelli.controllaNumeroCarta("123"));
+
+        // Test caso in cui il numero di carta non sia valido (contiene caratteri non numerici)
+        assertFalse(gestoreCarrelli.controllaNumeroCarta("123456789012345a"));
+    }
+    @Test
+    public void testCalcolaPrezzoTotale() {
         
-    	DbProdotti dbProdottiMock = new DbProdotti();
+    	DbProdotti dbProdottiMock = new DbProdotti(); 
         GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdottiMock);
 
-        System.out.println("inizio il test");
         ArrayList<HashMap<String, Object>> carrello = new ArrayList<>();
         HashMap<String, Object> prodotto1 = new HashMap<>();
-        prodotto1.put("prodotto123",  10.0); 
-        prodotto1.put("quantit√†",  2);
+        prodotto1.put("prodotto123", 10.0); 
+        prodotto1.put("quantit‡", 2);
         carrello.add(prodotto1);
 
-        ArrayList<String> prodotti = new ArrayList<>();
+        List<String> prodotti = new ArrayList<>();
         prodotti.add("prodotto123");
+ 
+        float risultato = gestoreCarrelli.calcolaPrezzoTotale(carrello, prodotti);
 
-      
-        /*float risultato = gestoreCarrelli.calcolaPrezzoTotale( carrello, prodotti);
-      System.out.println("il risultato : "+risultato);
+        boolean risult;
+        if(risultato==20)
+        	risult=true;
+        else
+        	risult= false;
         
-        assertTrue(risultato== 20.0); // Sostituisci con il risultato atteso*/
+        assertTrue(risult);
     }
-
     
+    @Test
+    public void testAggiornaOrdini() throws RemoteException {
+        DbProdotti dbProdottiMock = new DbProdotti();
+
+        GestoreCarrelli gestoreCarrelli = new GestoreCarrelli(dbProdottiMock);
+
+        ArrayList<HashMap<String, Object>> listaProdottiCarrello = new ArrayList<>();
+        HashMap<String, Object> prodotto1 = new HashMap<>();
+        prodotto1.put("username", "testUser");
+        prodotto1.put("codiceProdotto", 1);
+        prodotto1.put("quantitaProdotto", 2);
+        listaProdottiCarrello.add(prodotto1);
+
+        try {
+            gestoreCarrelli.aggiornaOrdini(listaProdottiCarrello, "2023-11-01");
+        } catch (RemoteException e) {
+            fail("Eccezione non attesa: " + e.getMessage());
+        }
+
+        assertFalse(listaProdottiCarrello.isEmpty());
+
+    }
 }
 
